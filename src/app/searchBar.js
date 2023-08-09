240bc13d2ca45d5d2b62008afeb3d2ad2b92c65f
Abderahmane Bouziane <bobmatt911@gmail.com>
2023-08-08 23:32:12 -0400

    fix: small refactor

--- src/app/searchBar.js
+++ src/app/searchBar.js
@@ -1,43 +1,49 @@
 'use client'
-
 import debounce from 'lodash.debounce';
 import { useState } from 'react';
 
 async function wait(n) {
-    return new Promise((res, _) => {
-        setTimeout(() => {
-            res();
-        }, n)
-    })
+  return new Promise((res, _) => {
+    setTimeout(() => {
+      res();
+    }, n)
+  })
 }
 
 const PODCASTS = ["sup", "sup2", "joe rogan", "american life", "studio 404"]
 
 async function getPodcasts(term) {
-    await wait(500);
+  await wait(500);
 
-    return PODCASTS.filter(e => e.includes(term));
+  return PODCASTS.filter(e => e.includes(term));
 }
 
 const SearchBar = () => {
-    const [loading, setLoading] = useState(false);
-    const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
 
-    const handleValue = debounce(async (value) => {
-        setLoading(true)
-        const res = await getPodcasts(value);
-        setLoading(false)
-        setSearchResults(res);
-    }, 1000)
+  async function search(term) {
+    setLoading(true)
+    const res = await getPodcasts(term);
+    setLoading(false)
+    setSearchResults(res);
+  }
 
-    return <>
-        <input type='text' placeholder='podcast or {url : todo define more}' onInput={(e) => { handleValue(e.target.value) }} />
-        {loading ? 'loading' :
-            <ul>
-                {searchResults.map((e, i) =>
-                    <li key={i}>{e}</li>
-                )}
-            </ul>}
-    </>
+  return <>
+    <input
+      type='text'
+      placeholder='podcast or {url : todo define more}'
+      onInput={
+        debounce((e) => search(e.target.value), 1000)
+      }
+    />
+    {loading ?
+      'loading' :
+      <ul>
+        {searchResults.map((e, i) =>
+          <li key={i}>{e}</li>
+        )}
+      </ul>}
+  </>
 }
 export default SearchBar
