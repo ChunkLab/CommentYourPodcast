c811366c8ec050131db7f832a8e74565c3873a5b
Abderahmane Bouziane <bobmatt911@gmail.com>
2023-08-23 10:31:34 -0400

    feat: run prettier on whole codebase

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import SearchBar from './searchBar'
 
 export default function Home() {
-
-
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <SearchBar></SearchBar>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            <SearchBar></SearchBar>
+        </main>
+    )
 }
