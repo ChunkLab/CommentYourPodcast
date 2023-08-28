787bd210da496fa15126dc96fcd0816dac8cfd54
alxeandre <alexandre.mr.billerau@gmail.com>
2023-08-28 17:39:49 -0400

    style : add circle to episodes page

--- src/app/podcast/[podcast]/episode/[episode]/client.js
+++ src/app/podcast/[podcast]/episode/[episode]/client.js
@@ -18,7 +18,9 @@ export default function Client({ episode_data }) {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="w-full max-w-xlg py-8 flex flex-row items-center justify-center mx-auto bg-[#FFFBFB] rounded-lg shadow-xl">
+      <div className='circle circle-orange'></div>
+      <div className='circle circle-blue'></div>
+      <div className="w-full max-w-xlg py-8 flex flex-row items-center justify-center mx-auto bg-[#FFFBFB] rounded-lg shadow-xl z-10">
         <div className="flex flex-row w-5/6 space-x-8">
           <div className="w-2/5 flex flex-col items-center justify-center">
             <figure className="w-full  rounded-full overflow-hidden">
@@ -54,13 +56,13 @@ export default function Client({ episode_data }) {
       <input
         ref={authorInputRef}
         placeholder="author.."
-        className="py-3 px-4 w-1/4 mt-10 rounded shadow font-thin focus:outline-none focus:shadow-lg focus:shadow-slate-200 duration-100 shadow-gray-100"
+        className="z-10 py-3 px-4 w-1/4 mt-10 rounded shadow font-thin focus:outline-none focus:shadow-lg focus:shadow-slate-200 duration-100 shadow-gray-100"
         type="text"
       />
       <textarea
         ref={contentInputRef}
         placeholder="comment.."
-        className="py-3 px-4 w-1/3 mt-3 rounded shadow font-thin focus:outline-none focus:shadow-lg focus:shadow-slate-200 duration-100 shadow-gray-100"
+        className="z-10 py-3 px-4 w-1/3 mt-3 rounded shadow font-thin focus:outline-none focus:shadow-lg focus:shadow-slate-200 duration-100 shadow-gray-100"
         type="text"
       />
       <button
@@ -71,11 +73,11 @@ export default function Client({ episode_data }) {
             createdAt: new Date().getTime() / 1000,
           })
         }}
-        className="transition-colors mt-3 bg-purple-700 hover:bg-purple-800 p-2 rounded-sm w-1/5 text-white text-hover shadow-md shadow-purple-900"
+        className="z-10 transition-colors mt-3 bg-purple-700 hover:bg-purple-800 p-2 rounded-sm w-1/5 text-white text-hover shadow-md shadow-purple-900"
       >
         Comment
       </button>
-      <ul>
+      <ul className='z-10'>
         {episodeData.comments
           .sort((e) => e.createdAt)
           .reverse()
