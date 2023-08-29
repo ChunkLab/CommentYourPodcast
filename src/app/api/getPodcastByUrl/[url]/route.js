5f6219a916c9c79815b673a7d148b3f31f093f2d
alxeandre <alexandre.mr.billerau@gmail.com>
2023-08-29 19:20:25 -0400

    feat: add route to get podcast buy url and save to db

---
+++ src/app/api/getPodcastByUrl/[url]/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/app/db'
+
+import { XMLParser } from 'fast-xml-parser'
+
+export async function GET(_, { params }) {
+  const res = await fetch(params.url)
+  const xml = await res.text()
+
+  const parser = new XMLParser()
+  let podcast_data = parser.parse(xml).rss.channel
+
+  const podcast = await prisma.podcast.create({
+    data: {
+      title: podcast_data.title,
+      description: podcast_data.description,
+      img: podcast_data.image.url,
+    },
+  })
+
+  const episodes = podcast_data.item.map((e) => ({
+    title: e.title,
+    description: e.description,
+    img: '',
+    duration: e['itunes:duration'],
+    createdAt: Date.parse(e.pubDate) / 1000,
+    podcastId: podcast.id,
+  }))
+  await prisma.episode.createMany({ data: episodes })
+
+  return NextResponse.json(podcast_data)
+}
